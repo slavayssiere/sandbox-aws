ac75936bc1900f25f7fe675c0584dc2487239c19
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2018-09-21 13:56:50 +0200

    add traefik

--- terraform/layer-kubernetes/install-bastion.sh
+++ terraform/layer-kubernetes/install-bastion.sh
@@ -8,7 +8,13 @@ wget -O kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+wget -O helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-rc.4-linux-amd64.tar.gz
+tar -xf helm.tar.gz
+chmod +x linux-amd64/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
+
 
 echo "export KOPS_STATE_STORE=s3://wescale-slavayssiere-kops" >> /home/ec2-user/.bashrc
-echo "kops export kubecfg test.slavayssiere.wescale" >> /home/ec2-user/.bashrc
+echo "kops export kubecfg test.slavayssiere.wescale >> /dev/null 2>&1" >> /home/ec2-user/.bashrc
+
 
--- terraform/layer-servicemesh/deploy.sh
+++ terraform/layer-servicemesh/deploy.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/prometheus-operator/v0.19.0.yaml
+cd ../layer-kubernetes
+bastion_hostname=$(terraform output bastion_public_dns)
+cd ../layer-servicemesh
+
+scp -r -oStrictHostKeyChecking=no ./manifests ec2-user@$bastion_hostname:~
+scp -r -oStrictHostKeyChecking=no ./traefik ec2-user@$bastion_hostname:~
+
+ssh -oStrictHostKeyChecking=no ec2-user@$bastion_hostname 
+
+# https://raw.githubusercontent.com/kubernetes/kops/master/addons/prometheus-operator/v0.19.0.yaml
+
+cd manifests
+kubectl create -f .
+kubectl apply -f v0.19.0.yaml
+cd ..
+
+cd traefik
+kubectl create -f .
+cd ..
+
+helm init --service-account tiller
+
