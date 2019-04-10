43e0c6f75e8f5856cd71133fe19ca47b39e9d1e1
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-04-10 14:42:24 +0200

    update

--- iac/layer-services/install.sh
+++ iac/layer-services/install.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 
-# https://raw.githubusercontent.com/kubernetes/kops/master/addons/prometheus-operator/v0.19.0.yaml
+test_tiller_present() {
+    kubectl get pod -n kube-system -l app=helm,name=tiller | grep Running | wc -l | tr -d ' '
+}
 
 until kops validate cluster
 do
@@ -7,23 +10,50 @@ do
     sleep 10
 done
 
-kubectl annotate ns kube-system iam.amazonaws.com/permitted=".*"
+kubectl apply -f helm/rbac.yaml
+helm init --service-account tiller # --tiller-tls-verify
+
+test_tiller=$(test_tiller_present)
+while [ $test_tiller -lt 1 ]; do
+    echo "Wait for Tiller: $test_tiller"
+    test_tiller=$(test_tiller_present)
+    sleep 1
+done
+
+sleep 10
+
+cd cluster-autoscaler
+kubectl create ns cluster-autoscaler
+helm install --namespace cluster-autoscaler --name aws-autoscaler stable/aws-cluster-autoscaler -f values.yaml
+cd ..
 
 cd rook
-kubectl apply -f operator.yaml
+helm repo add rook-stable https://charts.rook.io/stable
+kubectl create ns rook-ceph-system
+helm install --namespace rook-ceph-system --name rook-default -f values.yaml rook-stable/rook-ceph
 kubectl apply -f cluster.yaml
 kubectl apply -f pool.yaml
 cd ..
 
 cd prometheus-operator
-kubectl apply -f namespace-monitoring.yaml
-kubectl apply -f prometheus-operator.yaml
+kubectl create ns monitoring
+mkdir tmp && cd tmp
+wget https://raw.githubusercontent.com/coreos/prometheus-operator/master/contrib/kube-prometheus/manifests/0prometheus-operator-0prometheusCustomResourceDefinition.yaml
+wget https://raw.githubusercontent.com/coreos/prometheus-operator/master/contrib/kube-prometheus/manifests/0prometheus-operator-0alertmanagerCustomResourceDefinition.yaml
+wget https://raw.githubusercontent.com/coreos/prometheus-operator/master/contrib/kube-prometheus/manifests/0prometheus-operator-0prometheusruleCustomResourceDefinition.yaml
+wget https://raw.githubusercontent.com/coreos/prometheus-operator/master/contrib/kube-prometheus/manifests/0prometheus-operator-0servicemonitorCustomResourceDefinition.yaml
+kubectl apply -f .
+cd - && rm -Rf tmp
+
+helm install --namespace monitoring --name prom-operator -f values.yaml stable/prometheus-operator
 kubectl apply -f .
 cd ..
 
 cd kube-prometheus
-kubectl -n monitoring create secret generic alertmanager-main --from-file=alertmanager.yaml
-kubectl apply -f .
+# https://github.com/helm/charts/tree/master/stable/prometheus-operator
+kubectl -n monitoring create secret generic alertmanager-alertmanager-k8s --from-file=alertmanager.yaml
+kubectl apply -f prometheus-k8s.yaml
+kubectl apply -f alertmanager-k8s.yaml
 cd ..
 
 cd manifests
@@ -42,73 +72,14 @@ cd traefik-app
 kubectl apply -f .
 cd ..
 
-cd monitoring
+cd ingress-monitoring
 kubectl apply -f .
 cd ..
 
-kubectl apply -f cm-adapter-serving-certs.yaml -n monitoring
+## apps
 
-cd custom-metric
-kubectl apply -f .
-cd ..
-
-# cd kiam
-# kubectl create secret generic kiam-server-tls -n kube-system \
-#   --from-file=ca.pem \
-#   --from-file=server.pem \
-#   --from-file=server-key.pem
-
-# kubectl create secret generic kiam-agent-tls -n kube-system \
-#   --from-file=ca.pem \
-#   --from-file=agent.pem \
-#   --from-file=agent-key.pem
-
-# kubectl apply -f .
-# cd ..
-
-helm init --service-account tiller
-
-# helm repo add svc-cat https://svc-catalog-charts.storage.googleapis.com
-# helm repo add aws-sb https://awsservicebroker.s3.amazonaws.com/charts
-
-# helm install svc-cat/catalog \
-#     --name catalog \
-#     --namespace catalog
-
-# helm install aws-sb/aws-servicebroker \
-#     --name aws-servicebroker \
-#     --namespace aws-sb \
-#     --version 1.0.0-beta.2 \
-#     -f ./service-broker/values.yaml
-
-# helm install incubator/jaeger \
-#     --name myrel \
-#     --set provisionDataStore.cassandra=false \
-#     --set provisionDataStore.elasticsearch=false \
-#     --set storage.type=elasticsearch \
-#     --set storage.elasticsearch.host=$ES_HOST \
-#     --set storage.elasticsearch.port=443
-
-# curl -XPOST $ES_HOST/api/saved_objects/index-pattern -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @kibana/default-index-pattern.json
-
-# curl -XPOST -H 'Content-Type: application/json' \
-#   "$ES_HOST/.kibana/index-pattern/applogs-*" \
-#   -d'{"title":"applogs-*","timeFieldName":"@timestamp","notExpandable":true}'
-
-# curl -XDELETE -H 'Content-Type: application/json' \
-#   "$ES_HOST/.kibana/index-pattern/applogs-*"
-
-# curl -f -XPOST -H 'Content-Type: application/json' \
-#     -H 'kbn-xsrf: anything' \
-#     "$ES_HOST/api/saved_objects/index-pattern/applogs-*" \
-#     -d'{"attributes":{"title":"applogs-*","timeFieldName":"@timestamp"}}'
-
-# curl -XPOST "$ES_HOST/api/saved_objects/index-pattern/applogs-*" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @kibana/default-index-pattern.json
-
-# curl "$ES_HOST/api/saved_objects/index-pattern/applogs-*" -H "Content-Type: application/json" -H "kbn-xsrf: true"
-
-# # the traefik webui
-# htpasswd -bc ./auth traefik password
-# kubectl create secret generic traefik-login --from-file auth -n kube-system
-# kubectl apply -f traefik-web-ui.yaml
-
+cd mysql-operator
+kubectl create ns mysql-operator
+helm repo add presslabs https://presslabs.github.io/charts
+helm install --namespace mysql-operator presslabs/mysql-operator --name mysql-operator -f values.yaml --version 0.2.4
+cd -
--- iac/layer-services/reverse-proxy/launch-proxy.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run --name my-custom-nginx-container -v ./nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
